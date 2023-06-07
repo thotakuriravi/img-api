6e847ef6bd463f31346694af0bd3a4b7daf941a9
Thotakuri Ravi Kumar <78135736+thotakuriravi@users.noreply.github.com>
2023-06-07 10:08:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('templates/index.html')
 
 @app.route('/convert', methods=['POST'])
 def convert_text_to_image():
